3c92c3fc4ed403ff52b47b0419cbc0454184664e
Riteshgupta9310 <115911203+Riteshgupta9310@users.noreply.github.com>
2022-12-29 23:49:08 +0530

    Add files via upload

---
+++ class-comment-cipherschool.py
@@ -0,0 +1,13 @@
+#comment is something that you don't want to print in programme by using #
+print("hello \"world\" world")
+print('I\'m ritesh')
+#backslash n 
+print("line A\nline B\nline C")
+#backslash t
+print("Name\tRitesh")
+#backslash
+print("This is backslash\\")
+#how to use double backslash 
+print("this is blackslash\\\\")
+#backslash b
+print("hell\blo")
