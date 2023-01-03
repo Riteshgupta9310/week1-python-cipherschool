1a60ac976526f0d431955b03fb370853660249b5
Riteshgupta9310 <115911203+Riteshgupta9310@users.noreply.github.com>
2023-01-03 15:19:19 +0530

    Add files via upload

---
+++ class1-solutionsEX1-cipherSchool.py
@@ -0,0 +1,8 @@
+# this is \\ double backslash
+print("this is \\ backslash")
+# these are /\/\/\/\/\ mountains
+print("these are /\\/\\/\\/\\/\\ mountains")
+#he is  awesome (use escape sequence instead of manual space )
+print("he is\tawesome")
+#\" \n \t \' (print this as an output)
+print("\\\" \\n \\t \\\'")
